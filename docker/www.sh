a11ee8400b3e7cca3d0e81e7d452dd38c0b140e1
squawkcpp <squawkcpp@gmail.com>
2017-10-11 23:06:21 +0000

    fix configuration files

---
+++ docker/www.sh
@@ -0,0 +1,24 @@
+#! /bin/bash -e
+
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
+
+OPTIONS=""
+
+if [ -n "$LISTEN" ]; then
+    OPTIONS="$OPTIONS --listen $LISTEN"
+fi
+
+if [ -n "$HTTP_PORT" ]; then
+    OPTIONS="$OPTIONS --http-port $HTTP_PORT"
+fi
+
+if [ -n "$CDS_URI" ]; then
+    OPTIONS="$OPTIONS --cds $CDS_URI"
+fi
+
+if [ -n "$DOCROOT" ]; then
+    OPTIONS="$OPTIONS --docroot DOCROOT"
+fi
+
+echo "start /usr/local/bin/squawk-www $OPTIONS"
+/usr/local/bin/squawk-www $OPTIONS
--- script/www.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash -e
-
-PATH=/bin:/usr/bin:/sbin:/usr/sbin
-
-/usr/local/bin/squawk-www $WWW_OPTS
-
-tail -f /var/log/dmesg
