8fb1364d892e99e4d5c60c0c5e0f4e59ad51f2df
etienne <squawkcpp@gmail.com>
2017-12-24 21:20:18 +0100

    update build file

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ sudo docker pull spielhuus/clang:latest
 sudo docker run -itd --name build_www -v $(pwd):/repo -v $(pwd)/build:/build spielhuus/clang
 sudo docker exec build_www /usr/sbin/pacman --noconfirm -S boost doxygen make git
 
-sudo docker exec build_www cmake -H/repo -B/build -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja -DCDS_TAG_VERSION=master
+sudo docker exec build_www cmake -H/repo -B/build -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja -DWWW_TAG_VERSION=master
 sudo docker exec build_www cmake --build /build
 sudo docker exec build_www cmake --build /build --target package
 
-sudo docker build -f docker/Dockerfile --build-arg CDS_TAG_VERSION=master -t www .
+sudo docker build -f docker/Dockerfile --build-arg WWW_TAG_VERSION=master -t www .
 
 sudo docker rm -f build_www
 sudo rm -rf build
