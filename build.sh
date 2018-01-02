9d4eda289e39432883e89dbefe585b846c98373c
etienne <squawkcpp@gmail.com>
2018-01-03 00:41:00 +0100

    migrate to conan

--- build.sh
+++ build.sh
@@ -2,17 +2,14 @@
 
 set -e
 
-sudo docker pull spielhuus/clang:latest
-sudo docker run -itd --name build_www -v $(pwd):/repo -v $(pwd)/build:/build spielhuus/clang
+sudo docker run -it -v $(pwd):/repo -v $(pwd)/.build:/build spielhuus/toolchain /bin/bash -c 'cd /build && \
+	conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan && \
+	conan remote add conan-cpp https://api.bintray.com/conan/squawkcpp/conan-cpp && \
+	conan install /repo --build=missing -s compiler.libcxx=libstdc++11 && \
+	cmake -H/repo -B/build -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja -DWWW_TAG_VERSION=master
+	cmake --build /build && \
+	cmake --build /build --target package'
 
-sudo docker exec build_www cmake -H/repo -B/build -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja -DWWW_TAG_VERSION=master
-sudo docker exec build_www cmake --build /build
-sudo docker exec build_www cmake --build /build --target package
+sudo docker build -f docker/Dockerfile -t squawk-www .
 
-sudo docker build -f docker/Dockerfile --build-arg WWW_TAG_VERSION=master -t squawk-www .
-
-sudo docker rm -f build_www
-sudo rm -rf build
-
-
-
+sudo rm -rf .build
