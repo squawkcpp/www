c582c57d8c2ca6031087a2c8275c5e8b8f492009
etienne <squawkcpp@gmail.com>
2018-01-04 00:34:13 +0100

    update build files

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ echo "build squawk-www tag:$TAG, image:$PID"
 DOCKER_EXEC="sudo docker exec $PID /bin/sh -c"
 
 $DOCKER_EXEC "cd build && conan install /repo --build=missing -s compiler.libcxx=libstdc++11"
-$DOCKER_EXEC "cmake -H/repo -B/build -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja -Dbuild_tests=on -Dbuild_example_server=on -Dbuild_documentation=on -WWW_TAG_VERSION=$TAG"
+$DOCKER_EXEC "cmake -H/repo -B/build -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja -DWWW_TAG_VERSION=$TAG"
 $DOCKER_EXEC "cmake --build /build"
 $DOCKER_EXEC "cmake --build /build --target package"
 
