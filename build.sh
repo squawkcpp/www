f8be20435b5b921882963f105aacf42db0a39a74
etienne <squawkcpp@gmail.com>
2018-01-04 23:50:05 +0100

    update build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 IMAGE=spielhuus/toolchain
 TAG=`if [ -z "$1" ]; then echo "master"; else echo "$1" ; fi`
 PID=$(sudo docker run -itd -v $(pwd):/repo -v $(pwd)/.build:/build $IMAGE /bin/sh)
-echo "build squawk-www tag:$TAG, image:$PID"
+echo "build $REPO tag:$TAG, image:$PID"
 
 DOCKER_EXEC="sudo docker exec $PID /bin/sh -c"
 
@@ -15,5 +15,3 @@ $DOCKER_EXEC "cmake --build /build"
 $DOCKER_EXEC "cmake --build /build --target package"
 
 #sudo docker build -f docker/Dockerfile --build-arg WWW_TAG_VERSION=$TAG -t squawk-www .
-
-sudo docker rm -f $PID
