08b6cdf3f61782b3436bec89b2a36f765b038a55
etienne <squawkcpp@gmail.com>
2018-01-04 23:42:53 +0100

    update build script

--- build.sh
+++ build.sh
@@ -14,6 +14,6 @@ $DOCKER_EXEC "cmake -H/repo -B/build -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninj
 $DOCKER_EXEC "cmake --build /build"
 $DOCKER_EXEC "cmake --build /build --target package"
 
-sudo docker build -f docker/Dockerfile --build-arg WWW_TAG_VERSION=$TAG -t squawk-www .
+#sudo docker build -f docker/Dockerfile --build-arg WWW_TAG_VERSION=$TAG -t squawk-www .
 
 sudo docker rm -f $PID
