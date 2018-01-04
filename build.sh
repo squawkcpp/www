6d124de70c493f241a2953148c7da9dfc79ea4ca
etienne <squawkcpp@gmail.com>
2018-01-05 00:06:15 +0100

    build dockerfile

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@ set -e
 
 IMAGE=spielhuus/toolchain
 TAG=`if [ -z "$1" ]; then echo "master"; else echo "$1" ; fi`
-PID=$(sudo docker run -itd -v $(pwd):/repo -v $(pwd)/.build:/build $IMAGE /bin/sh)
-echo "build $REPO tag:$TAG, image:$PID"
+PID=$(sudo docker run -itd -v $(pwd):/repo -v $(pwd)/.build:/.build $IMAGE /bin/sh)
+echo "build squawk-www tag:$TAG, image:$PID"
 
 DOCKER_EXEC="sudo docker exec $PID /bin/sh -c"
 
-$DOCKER_EXEC "cd build && conan install /repo --build=missing"
-$DOCKER_EXEC "cmake -H/repo -B/build -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja -DWWW_TAG_VERSION=$TAG"
-$DOCKER_EXEC "cmake --build /build"
-$DOCKER_EXEC "cmake --build /build --target package"
+$DOCKER_EXEC "cd .build && conan install /repo --build=missing"
+$DOCKER_EXEC "cmake -H/repo -B/.build -G Ninja -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja -DWWW_TAG_VERSION=$TAG"
+$DOCKER_EXEC "cmake --build /.build"
+$DOCKER_EXEC "cmake --build /.build --target package"
 
-#sudo docker build -f docker/Dockerfile --build-arg WWW_TAG_VERSION=$TAG -t squawk-www .
+sudo docker build -f docker/Dockerfile --build-arg WWW_TAG_VERSION=$TAG -t squawk-www .
